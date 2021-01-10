95b98aec4c0834ed9993005046d0e09ca19fba90
R_Vlad <shootniky@gmail.com>
2021-01-10 19:06:22 +0300

    [upd] It is homework - Lesson 3 - task 1

--- .idea/lesson_3/les3_task_1.py
+++ .idea/lesson_3/les3_task_1.py
@@ -1,2 +1,30 @@
 # 1. Реализовать функцию, принимающую два числа (позиционные аргументы) и выполняющую их деление.
 # Числа запрашивать у пользователя, предусмотреть обработку ситуации деления на ноль.
+
+def del_func ():
+    first_number = int(input("Enter first number: "))
+    second_number = int(input("Enter second number: "))
+    try:
+        res = first_number / second_number
+        return res
+    except ZeroDivisionError:
+        print("Error! ZERO!")
+calc_del = del_func()
+print(f"Two positions arguments divide first / second  =  {calc_del}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
